c83bd76e033036f7d738cd02cce52e68b44abcc1
Mojtaba Amirabadian <mamirabadian@gmail.com>
2024-04-01 02:37:39 +0330

    Dynamic currency list

--- lib/widgets/CurrencyWidget.dart
+++ lib/widgets/CurrencyWidget.dart
@@ -22,90 +22,91 @@ class _CurrencyWidgetState extends State<CurrencyWidget> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Obx(
-                  () => controller.isLoading.value
-                  ? const Center(
-                    child: CircularProgressIndicator(),
+              () => controller.isLoading.value
+              ? const Center(
+                child: CircularProgressIndicator(),
               )
-                  : ListView.builder(
-                  itemCount: 10,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 15),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 60,
-                        child: Row(
-                          mainAxisAlignment:
-                          MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Padding(
-                                  padding:
-                                  const EdgeInsets.only(right: 10),
-                                  child: Container(
-                                    width: 60,
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                        color: Colors.grey[700],
-                                        borderRadius:
-                                        BorderRadius.circular(15),
-                                        boxShadow: [
-                                          BoxShadow(
-                                              color: Colors.grey[700]!,
-                                              offset: const Offset(4, 4),
-                                              blurRadius: 5)
-                                        ]),
-                                    child: const Padding(
-                                      padding: EdgeInsets.all(10),
-                                      // child:
-                                      // Image.network(controller
-                                      //     .currencyList[index].image),
-                                    ),
+              : ListView.builder(
+                itemCount: controller.currencyList.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 60,
+                      child: Row(
+                        mainAxisAlignment:
+                        MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Padding(
+                                padding:
+                                const EdgeInsets.only(right: 10),
+                                child: Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[700],
+                                      borderRadius:
+                                      BorderRadius.circular(15),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: Colors.grey[700]!,
+                                            offset: const Offset(4, 4),
+                                            blurRadius: 5)
+                                      ]),
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(10),
+                                    // child:
+                                    // Image.network(controller
+                                    //     .currencyList[index].image),
                                   ),
                                 ),
-                                Column(
-                                  crossAxisAlignment:
-                                  CrossAxisAlignment.start,
-                                  children: [
-                                    const SizedBox(
-                                      height: 5,
-                                    ),
-                                    Text(
-                                        controller.currencyList[index].name
-                                    ),
-                                    Text(
-                                      ' ${double.parse(controller.currencyList[index].priceChange)} %',
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  '\$ ${controller.currencyList[index].currentPrice}',
-                                ),
-                                // Text(
-                                //   controller.currencyList[index].symbol
-                                //       .toUpperCase(),
-                                // )
-                              ],
-                            )
-                          ],
-                        ),
+                              ),
+                              Column(
+                                crossAxisAlignment:
+                                CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                      controller.currencyList[index].name
+                                  ),
+                                  Text(
+                                    ' ${double.parse(controller.currencyList[index].priceChange)} %',
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                '\$ ${controller.currencyList[index].currentPrice}',
+                              ),
+                              // Text(
+                              //   controller.currencyList[index].symbol
+                              //       .toUpperCase(),
+                              // )
+                            ],
+                          )
+                        ],
                       ),
-                    );
-                  }),
+                    ),
+                  );
+                }
+              ),
             ),
           ],
         ),
